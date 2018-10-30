4fe0539f68ed050f37adb03a0b6b24da7c8ff484
Wheatcake <44545331+Wheatcake@users.noreply.github.com>
2018-10-30 10:01:52 +0000

    Helmet Update
    +helmet Swat

--- config.cpp
+++ config.cpp
@@ -201,7 +201,8 @@ class CfgPatches
 			"SCI_helmet_Husky",
 			"SCI_helmet_Reynard",
 			"SCI_helmet_Hazza",
-			"SCI_helmet_BMyint"
+			"SCI_helmet_BMyint",
+			"SCI_helmet_Swat"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]=
@@ -3275,6 +3276,14 @@ class cfgWeapons
 		hiddenSelectionsTextures[]=
 		{
 			"sci_cloneArmor\data\helmettextures\p2helmet_BMyint_CO.paa"
+		};
+	};
+	class SCI_helmet_Swat: SCI_helmet_base
+	{
+		displayName="[SCI] ACH-P-2 'Swat'";
+		hiddenSelectionsTextures[]=
+		{
+			"sci_cloneArmor\data\helmettextures\p2helmet_Swat_CO.paa"
 		};
 	};
 	class SCI_helmet_Replace: SCI_helmet_base
